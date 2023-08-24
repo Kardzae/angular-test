2fd89b5d837c4522c9b0dbeb136918104eb3d2b3
kardzae <emmanuelpp21@gmail.com>
2023-08-24 16:48:20 -0500

    primer commit

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,26 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'autor',
+        data: { pageTitle: 'proyectoFront2App.autor.home.title' },
+        loadChildren: () => import('./autor/autor.routes'),
+      },
+      {
+        path: 'cliente',
+        data: { pageTitle: 'proyectoFront2App.cliente.home.title' },
+        loadChildren: () => import('./cliente/cliente.routes'),
+      },
+      {
+        path: 'libro',
+        data: { pageTitle: 'proyectoFront2App.libro.home.title' },
+        loadChildren: () => import('./libro/libro.routes'),
+      },
+      {
+        path: 'venta',
+        data: { pageTitle: 'proyectoFront2App.venta.home.title' },
+        loadChildren: () => import('./venta/venta.routes'),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
